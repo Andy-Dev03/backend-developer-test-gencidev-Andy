95ab2e878ce17f2f5e536ce92250ba0715012ba0
Rexdust <andy0412lie@gmail.com>
2025-08-23 22:29:11 +0700

    feat: create Models | utils (bcryptjs & jwt)

---
+++ express/migrations/20250823150903-create-note.js
@@ -0,0 +1,42 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Notes", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Notes");
+  },
+};
---
+++ express/utils/jwt.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const secretKey = process.env.SECRET_KEY;
+
+const generateToken = (payload) => {
+  return jwt.sign(payload, secretKey, { expiresIn: "12h" });
+};
+
+const verifyToken = (token) => {
+  return jwt.verify(token, secretKey);
+};
+
+module.exports = {
+  generateToken,
+  verifyToken,
+};
